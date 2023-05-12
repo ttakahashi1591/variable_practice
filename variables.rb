930150f27d795a92b325936ab4a8c6d3a1d3a472
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-05-12 16:32:22 -0600

    Reassign strings and add puts statements

--- variables.rb
+++ variables.rb
@@ -1,5 +1,14 @@
 email = "ttakahashi1591@gmail.com"
+email # "ttakahashi1591@gmail.com"
+
+email = "tjm71@humboldt.edu"
+email # "tjm71@humboldt.edu"
+
 address = "1811 S Quebec Way Apt 180"
+address # "1811 S Quebec Way Apt 180"
+
+address = "2517 W 232nd Street"
+address # "2517 W 232nd Street"
 
 age = 32
 days_to_turing = 50
@@ -9,3 +18,8 @@ hours_worked = 6.25
 
 currently online = true
 deposit_paid = true
+
+puts "ttakahashi1591@gmail.com"
+puts 32
+puts 29226.09
+puts true
